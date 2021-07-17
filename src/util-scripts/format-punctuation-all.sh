38045f9b8e32e9a7ad031d8fa0c907b1e7ac6749
masataka <yamauchi@ims.ac.jp>
2021-07-17 14:55:38 +0900

    スクリプトのディレクトリを変更

--- src/util-scripts/format-punctuation-all.sh
+++ src/util-scripts/format-punctuation-all.sh
@@ -2,6 +2,6 @@
 
 for file in `find . -name '*.tex' -and ! -name 'main.tex' -and ! -name 'preface.tex' -and ! -name 'title.tex'`
 do
-    sh ./scripts/format-punctuation.sh $file
+    sh ./util-scripts/format-punctuation.sh $file
     echo $file
 done
